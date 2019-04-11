1715d0352037b98301184606aa8cc9d4930f739d
Lauren <ljcrafty@comcast.net>
2019-04-11 16:05:28 -0400

    Skeleton Client and Server

---
+++ src/TCPServer.java
@@ -0,0 +1,14 @@
+
+
+/**
+    Creates and runs a server that accepts TCP connections
+ */
+public class TCPServer {
+    //instance variables
+    private int port;
+    
+    public TCPServer(int port)
+    {
+
+    }
+}
