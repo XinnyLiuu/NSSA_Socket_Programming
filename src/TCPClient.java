98ca9888aed2d9ebf5003bfd52e1b6eb745de335
Lauren <ljcrafty@comcast.net>
2019-04-11 16:05:28 -0400

    Skeleton Client and Server

---
+++ src/TCPClient.java
@@ -0,0 +1,22 @@
+
+/**
+    Creates and runs a client that makes TCP connections
+ */
+public class TCPClient
+{
+    //instance variables
+    private String IP, hostname;
+    private int port;
+
+    public TCPClient(String IP, int port)
+    {
+
+    }
+
+    public TCPClient(String hostname, int port)
+    {
+
+    }
+
+
+}
