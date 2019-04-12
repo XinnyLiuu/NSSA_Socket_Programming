6a527b0a1e8283327417168212b80158a34a58c6
Xin Liu <xhliu97@gmail.com>
2019-04-12 18:03:07 -0400

    finished udp server, may need testing on multiple client connections

--- src/UDP/UDPServer.java
+++ src/UDP/UDPServer.java
@@ -2,6 +2,8 @@ package UDP;
 
 import java.io.IOException;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 /**
  * UDP Server
@@ -29,7 +31,14 @@ public class UDPServer {
 			System.out.printf("Running UDP on Port %s %n", port);
 
 			// Start listening for data sent from client
-			init();
+			try {
+				init();
+			}
+			catch(IOException ioe) {
+				ioe.printStackTrace();
+				System.exit(1);
+			}
+
 		}
 		catch(SocketException se) {
 			se.printStackTrace();
@@ -44,48 +53,38 @@ public class UDPServer {
 	/**
 	 * Starts the server to listen to a client sending data over UDP
 	 */
-	private void init() {
+	private void init() throws IOException {
 		boolean stop = false; // Flag for stopping server
 		byte[] buffer = new byte[256]; // Maximum size of message to be received
 
 		while(!stop) {
+			// Get timestamp
+			String timestamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+
 			// Setup DatagramPacket to receive request
-			DatagramPacket data = new DatagramPacket( buffer, buffer.length);
-			try {
-				serverSocket.receive( data );
-			}
-			catch(IOException ioe) {
-				ioe.printStackTrace();
-				System.exit(1);
-			}
+			DatagramPacket request = new DatagramPacket( buffer, buffer.length);
+			serverSocket.receive( request );
 
-			// TODO: figure out what this means
-			String received = new String( data.getData(), 0, data.getLength());
+			// Get request information
+			InetAddress address = request.getAddress();
+			int port = request.getPort();
 
-			// Get data information
-			InetAddress address = data.getAddress();
-			int port = data.getPort();
-
-			// Setup DatagramPacket to send response b
-			data = new DatagramPacket( buffer, buffer.length, address, port );
-
-			// Check contents of received
-
+			// Check received data
+			String received = new String( request.getData(), 0, request.getLength());
+			System.out.printf("%s %s client %s %n", timestamp, received, address);
 			if(received.equals("end")) {
 				stop = true;
-				return;
 			}
 
-			// Send response
-			try {
-				serverSocket.send( data );
-			}
-			catch(IOException ioe) {
-				ioe.printStackTrace();
-				System.exit(1);
-			}
+			// Prepare echo with timestamp as response
+			String echo = timestamp + " " + received;
+			buffer = echo.getBytes();
 
-			// Clear buffer
+			// Setup DatagramPacket to send response (echo above)
+			DatagramPacket response = new DatagramPacket( buffer, buffer.length, address, port );
+			serverSocket.send( response );
+
+			// Clear buffer for next read
 			buffer = new byte[256];
 		}
 
